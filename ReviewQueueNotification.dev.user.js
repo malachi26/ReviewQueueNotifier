ef04f214d4bc1bc853a12f48c8982458ca463c74
malachi26 <malachi26@users.noreply.github.com>
2015-08-20 11:09:50 -0500

    Update ReviewQueueNotification.dev.user.js

--- ReviewQueueNotification.dev.user.js
+++ ReviewQueueNotification.dev.user.js
@@ -4,7 +4,7 @@
 // @author Malachi with help from Simon Forsberg
 // @description Shows a desktop notification when there review items in the queue. 
 // @namespace https://github.com/malachi26/SE-Scripts
-// @version 2.0.1 Beta
+// @version 2.0.1
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_notification
