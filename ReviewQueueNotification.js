49219538ccb82fb3557a632f4c6780471f01887e
malachi26 <malachi26@users.noreply.github.com>
2015-08-21 08:16:41 -0500

    Update ReviewQueueNotification.js

--- ReviewQueueNotification.js
+++ ReviewQueueNotification.js
@@ -4,9 +4,11 @@
 // @author Malachi with help from Simon Forsberg
 // @description Shows a desktop notification when there review items in the queue. 
 // @namespace https://github.com/malachi26/SE-Scripts
+// @version 2.1.1beta
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_notification
+// @grant Notifications
 // @match *://*.stackexchange.com/review
 // @match *://*.stackoverflow.com/review
 // @match *://*.mathoverflow.net/review
@@ -14,11 +16,14 @@
 // @match *://*.askubuntu.com/review
 // @match *://*.stackapps.com/review    
 // @match *://*.superuser.com/review
+// @icon http://a.fsdn.com/allura/p/greasemonkey/icon
 // ==/UserScript==
 */
 
+    Notification.requestPermission();
+
     var KEY_NEXT = 'NextReload';
-    var DELAY = 30 * 1000; //30,000 milliseconds
+    var DELAY = 15 * 1000; //30,000 milliseconds
     var currentTime = Date.now ? Date.now() : new Date().getTime();
     var lastTime = GM_getValue(KEY_NEXT, 0);
     var nextTime = currentTime + DELAY;
@@ -44,11 +49,11 @@
    
         if (reviewCount > 0) {
             var details = {
-                title: document.title,
-                text: reviewCount + ' Review Items',
-                timeout: 15000
-            }
-            GM_notification(details, null);
-        }
+                body: reviewCount + ' Review Items',
+                icon: 'http://icons-search.com/img/fasticon/icomic_lnx.zip/icomic_lnx-icons-32X32-web.png-32x32.png'
+            } 
+            var n = new Notification(document.title, details );
+            setTimeout(n.close.bind(n), 15000);            
+		    }
     }
 
