03d5902d6c09cfb27cd2c0060cbabdfe545bf99f
malachi26 <lyle.hart.3@gmail.com>
2015-09-03 16:09:56 -0500

    Moved Next Version to Current Version
    I am going to be starting new development on this Script

--- ReviewQueueNotification.user.js
+++ ReviewQueueNotification.user.js
@@ -4,7 +4,7 @@
 // @author Malachi with help from Simon Forsberg
 // @description Shows a desktop notification when there review items in the queue. 
 // @namespace https://github.com/malachi26/ReviewQueueNotifier
-// @version 2.1.3
+// @version 2.2.1 Next
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_notification
@@ -16,14 +16,14 @@
 // @match *://*.askubuntu.com/review
 // @match *://*.stackapps.com/review    
 // @match *://*.superuser.com/review
-// @icon https://github.com/malachi26/ReviewQueueNotifier/raw/master/Resources/Icon2.png
+// @icon https://github.com/malachi26/ReviewQueueNotifier/raw/master/Resources/Icon2.jpg
 // ==/UserScript==
 */
 
     Notification.requestPermission();
 
     var KEY_NEXT = 'NextReload';
-    var DELAY = 120 * 1000; //120,000 milliseconds = 2 minutes
+    var DELAY =  120 * 1000; //120,000 milliseconds = 2 Minutes
     var currentTime = Date.now ? Date.now() : new Date().getTime();
     var lastTime = GM_getValue(KEY_NEXT, 0);
     var nextTime = currentTime + DELAY;
@@ -34,8 +34,8 @@
         window.location.reload(); 
     }, DELAY);
 
-    document.title = document.title.split(' - ')[1] + ' Review Queue'; // keep the site name
-
+    var notificationTitle = (document.title.split(' - ')[1] + ' Review Queue').replace(' Stack Exchange', '.SE');
+    
     // a way to detect that the script is being executed because of an automatic script reload, not by the user.
     if (timeDiff <= DELAY * 2) {
         var reviewCount = 0;
@@ -53,9 +53,9 @@
         if (reviewCount > 0) {
             var details = {
                 body: reviewCount + ' Review Items',
-                icon: 'https://github.com/malachi26/ReviewQueueNotifier/raw/master/Resources/Icon2.png'
+                icon: 'https://github.com/malachi26/ReviewQueueNotifier/raw/master/Resources/Icon2.jpg'
             } 
-            var n = new Notification(document.title.replace(' Stack Exchange', '.SE'), details );
-            setTimeout(n.close.bind(n), 60000);            
-		    }
+            var n = new Notification(notificationTitle, details );
+            setTimeout(n.close.bind(n), 100000); // Magic number is time to notification disappear      
+	}
     }
