de416b1d6187e02083f3b5f72851e018568f5955
Lyle Hart III <lyle.hart@cellularsales.com>
2016-04-27 08:55:16 -0400

    for some reason I cannot read the site_url Property from the sites[i] object?

--- Extension/background.js
+++ Extension/background.js
@@ -2,23 +2,13 @@
 	$.getJSON("http://api.stackexchange.com/2.2/sites", function (data){
 		sites = data;
 	});
-	 
-	 // var sites = $.ajax({
-		// url:'http://api.stackexchange.com/2.2/sites',
-		// dataType: 'json',
-		// data: {},
-		// success: function(data, status, xhr) {
-			// return data;
-		// },
-		// error: function(xhr,status, error) {
-			// console.log(status.toString);
-		// }
-	// });	
+
 	console.dir(sites);
 	chrome.tabs.onUpdated.addListener(function(tab) {
-		for (var i = 0; i < sites.length; i++){
-			if (tab.url = sites.site_url[i] + '/review'){
-				chrome.pageAction.show(tab.id);
+
+		for (var i = 0; i < Object.keys(sites).length; i++){
+			if (tab.url == sites[i].site_url + '/review'){
+				chrome.pageaction.show(tab.id);
 			}
 		}
 	});
